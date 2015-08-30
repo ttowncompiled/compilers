99abf1dfaca0fcdc50c9a1ca060fc25ab55fd45f
ttowncompiled <ian.riley.915@gmail.com>
2015-08-29 23:14:59 -0500

    Defines the type annotations.

--- types.h
+++ types.h
@@ -79,4 +79,50 @@ typedef struct TokenNode {
   struct TokenNode* next;
 } TokenNode;
 
+char* annotation_of(int type) {
+  switch (type) {
+    case -1: return "(EOF)";
+    case 0: return "(NULL)";
+    case 1: return "(ADDOP)";
+    case 2: return "(MULOP)";
+    case 3: return "(ARRAY)";
+    case 4: return "(ASSIGNOP)";
+    case 5: return "(BEGIN)";
+    case 6: return "(CLOSEBRACKET)";
+    case 7: return "(CLOSEPAREN)";
+    case 8: return "(COLON)";
+    case 9: return "(COMMA)";
+    case 10: return "(MULOP)";
+    case 11: return "(DO)";
+    case 12: return "(ELSE)";
+    case 13: return "(END)";
+    case 14: return "(FUNCTION)";
+    case 15: return "(ID)";
+    case 16: return "(IF)";
+    case 17: return "(INTEGER)";
+    case 18: return "(MINUS)";
+    case 19: return "(MULOP)";
+    case 20: return "(MULOP)";
+    case 21: return "(NOT)";
+    case 22: return "(NUM)";
+    case 23: return "(OF)";
+    case 24: return "(OPENBRACKET)";
+    case 25: return "(OPENPAREN)";
+    case 26: return "(ADDOP)";
+    case 27: return "(PERIOD)";
+    case 28: return "(PLUS)";
+    case 29: return "(PROCEDURE)";
+    case 30: return "(PROGRAM)";
+    case 31: return "(RANGE)";
+    case 32: return "(REAL)";
+    case 33: return "(RELOP)";
+    case 34: return "(SEMICOLON)";
+    case 35: return "(THEN)";
+    case 36: return "(VAR)";
+    case 37: return "(WHILE)";
+    case 38: return "(LEXERR)";
+  }
+  return NULL;
+}
+
 #endif
