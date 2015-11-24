683f444d00f3580136670e90a5f5634b44cdebc3
Ian Riley <ian.riley.915@gmail.com>
2015-11-23 21:07:04 -0600

    Outputs listing file and hooks up lexer to the compiler.

--- src/compiler.go
+++ src/compiler.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "bufio"
+  "compiler"
   "container/list"
   "fmt"
   "io"
@@ -9,36 +10,46 @@ import (
   "path/filepath"
 )
 
+type Line struct {
+  number int
+  value string
+  errors *list.List
+}
+
 func check(e error) {
   if e != nil {
     panic(e)
   }
 }
 
-func write_listing_file(listing *list.List) {
-  line_number := 0
+func OutputListingFile(listing *list.List) {
   for e := listing.Front(); e != nil; e = e.Next() {
-    line_number++
-    fmt.Println(line_number, e.Value, "\n")
+    l := e.Value.(Line)
+    fmt.Println(l.number, l.value)
+    for e1 := l.errors.Front(); e1 != nil; e1 = e1.Next() {
+      fmt.Println(l.number, e1.Value)
+    }
+    fmt.Print("\n")
   }
 }
 
 func main() {
-  filepath, err := filepath.Abs(os.Args[1])
-  check(err)
-
-  file, err := os.Open(filepath)
-  check(err)
-  
+  filepath, e0 := filepath.Abs(os.Args[1])
+  check(e0)
+  file, e1 := os.Open(filepath)
+  check(e1)
+  defer file.Close()
   reader := bufio.NewReader(file)
-  listing_file := list.New()
-  
-  line, _, err := reader.ReadLine()
-  for err != io.EOF {
-    check(err)
-    line, _, err = reader.ReadLine()
-    listing_file.PushBack(string(line[:]))
+  listing := list.New()
+  line, _, e2 := reader.ReadLine()
+  lineNumber := 1
+  for e2 != io.EOF {
+    check(e2)
+    l := Line{lineNumber, string(line[:]), list.New()}
+    listing.PushBack(l)
+    line, _, e2 = reader.ReadLine()
+    lineNumber++
   }
-  
-  write_listing_file(listing_file)
+  compiler.Tokenize(listing)
+  OutputListingFile(listing)
 }
---
+++ src/compiler/lexer.go
@@ -0,0 +1,10 @@
+package compiler
+
+import (
+  "container/list"
+  "fmt"
+)
+
+func Tokenize(listing *list.List) {
+  fmt.Println("lexer not yet implemented")
+}
